218c5c7c68bf71cfc355e1a7393e775e86b4732c
Zell <fenglong1502@hotmail.com>
2025-05-27 09:05:52 +0800

    Setup Controller, Repository, Entity classes

---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(updatable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(updatable = false)
+    private String updatedBy;
+}
