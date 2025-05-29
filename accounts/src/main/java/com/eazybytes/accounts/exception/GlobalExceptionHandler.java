b701bc0a23008f0f44a96bd6e5fe4e00225e21c1
Zell <fenglong1502@hotmail.com>
2025-05-29 08:36:52 +0800

    Create update and delete endpoint and global exception handling

--- accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -11,6 +11,19 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalExceptino(Exception exception,
+                                                                            WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
@@ -22,7 +35,6 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
 
-        System.out.println("TEst11");
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
     }
 
--- accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -12,4 +12,8 @@ public interface IAccountsService {
     void createAccounts(CustomerDto customerDto);
 
     CustomerDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
 }
